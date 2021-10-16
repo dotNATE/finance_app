508107de26a960a5ae51f5582b6ee8a5a0ee0785
dotNate <somersetnathaniel@gmail.com>
2021-10-16 16:16:39 +0100

    added basic graphql schema and resolvers

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const express = require('express');
-const bodyParser = require('body-parser').json;
+const { json } = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
-app.use(bodyParser())
+app.use(json());
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            records: [String!]!
+        }
+
+        type RootMutation {
+            createRecord(name: String): String
+        }
+    
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        records: () => {
+            return ['january', 'february', 'march'];
+        },
+        createRecord: (args) => {
+            const recordName = args.name;
+
+            return recordName;
+        }
+    },
+    graphiql: true,
+}));
 
 app.listen(3000);
